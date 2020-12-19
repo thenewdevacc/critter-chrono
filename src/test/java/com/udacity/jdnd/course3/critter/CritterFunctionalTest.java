58969e90e84ba36eafaa4ee61f5cbc9cfba61ff5
Dev Account <thenewdevacc@gmail.com>
2020-12-19 20:32:41 +0530

    Added a test database

--- src/test/java/com/udacity/jdnd/course3/critter/CritterFunctionalTest.java
+++ src/test/java/com/udacity/jdnd/course3/critter/CritterFunctionalTest.java
@@ -11,6 +11,9 @@ import com.udacity.jdnd.course3.critter.user.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
